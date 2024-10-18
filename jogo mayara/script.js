df480710fc9ba795fc1cacf112640fae7a260a9b
Tufy4 <tfzinf@gmail.com>
2024-10-18 15:22:27 -0300

    avanço grande

--- jogo mayara/script.js
+++ jogo mayara/script.js
@@ -10,14 +10,15 @@ window.addEventListener("load", ()=>{
         "Poluição do solo": ["Uranio", "Chorume"]
     };
 
-    //Exemplo para a Minnie ver  
-    //"Poluição do ar": {"Enxofre": {"img":"./assets", "texto":"sda"}, "Chuva"};
 
+    //  
+    //"Poluição do ar": {"Enxofre": {"img":"./assets", "texto":"sda"}, "Chuva"};
     
     let tema_da_vez = TEMAS_PERMITIDOS[Math.round(Math.random() * (TEMAS_PERMITIDOS.length - 1))];
     
     let palavra_certa = PALAVRAS_PERMITIDAS[tema_da_vez][Math.round(Math.random() * (PALAVRAS_PERMITIDAS[tema_da_vez].length - 1))].toLowerCase();
-    
+
+
     const dica = document.querySelector("#cabecalho h3");
     dica.innerText = `O tema é: ${tema_da_vez}`;
     
@@ -40,6 +41,7 @@ window.addEventListener("load", ()=>{
         2:["Z", "X", "C", "V", "B", "N", "M"]
     }
     
+    
 
     teclado_linhas.forEach((linha, ind)=>{
         TECLADO_TECLAS[ind].forEach((letra)=>{
@@ -52,31 +54,72 @@ window.addEventListener("load", ()=>{
                 if(quantidade_erros < MAX_ERROS){
                     if(palavra_certa.includes(e.target.innerText.toLowerCase())){
                         e.target.classList = "tecla tecla_certa";
-                        for(let posicao = 0; posicao < linha_da_forca.childNodes.length;posicao++){
-                            if(palavra_certa[posicao]==e.target.innerText.toLowerCase()){
-                                console.log("ESTOU FUNCIONANDO")
-    
+
+
+                        for(let posicao = 0; posicao < linha_da_forca.childNodes.length; posicao++){
+                            if(palavra_certa[posicao] == e.target.innerText.toLowerCase()){
                                 linha_da_forca.children.item(posicao).innerText = e.target.innerText.toUpperCase();
                             }
                         }
                         
                     } 
-
-                    if(!palavra_certa.includes(e.target.innerText.toLowerCase()) && e.target.classList != "tecla tecla_errada") {
+        
+                    if( !palavra_certa.includes(e.target.innerText.toLowerCase()) && e.target.classList != "tecla tecla_errada") {
                         e.target.classList = "tecla tecla_errada";
 
                         const forca = document.querySelector("#container img");
+
                         quantidade_erros++;
-                        forca.setAttribute("src", "./assets/" + "erro" + quantidade_erros + ".png");
+                        
+                        forca.setAttribute("src", "./assets/erro" + quantidade_erros + ".png"); 
+                    
                     }
                 }
             })
 
             linha.appendChild(tecla);
-            
+
         })
     })
     // ---------------------------------------
+    const teclas = document.querySelectorAll("#teclado .linha .tecla");
+    
+    // TECLADO MANUAL ------------------------
+    window.addEventListener("keypress", (e)=>{
+        const tecla_manual = e.key.toLowerCase();
 
+        if(quantidade_erros < MAX_ERROS){   
+        linha_da_forca.childNodes.forEach((campo, ind)=>{
+            if(palavra_certa[ind] == tecla_manual){
+                campo.textContent = tecla_manual.toUpperCase();
+            }
+        })
+                
+        
+            if(palavra_certa.includes(tecla_manual)){
+            teclas.forEach((tecla)=>{
+                if(tecla.textContent.toLowerCase() == tecla_manual && tecla.classList != "tecla tecla_certa"){
+                    tecla.classList = "tecla tecla_certa";
+                }
+            })
 
+            } else {
+                teclas.forEach((tecla)=>{
+                    if(tecla.innerText.toLowerCase() == tecla_manual && tecla.classList != "tecla tecla_errada"){
+                        tecla.classList = "tecla tecla_errada";
+                    }
+                })
+
+                 const forca = document.querySelector("#container img");
+
+                quantidade_erros++;
+                        
+                forca.setAttribute("src", "./assets/erro" + quantidade_erros + ".png"); 
+            }
+    }
+    // -------------------------------------
+
+    
+
+    })
 })
